028b8a4434fa80242e0e55a93849a99761674dfc
JuanMrtt <juanantoniomartinb@gmail.com>
2023-04-18 17:35:17 +0200

    feat: services and types from api

---
+++ src/services/index.ts
@@ -0,0 +1,15 @@
+import { type Feed } from '../models/types';
+
+const API_URL =
+  'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json';
+
+export const fetchFeed = async (): Promise<Feed> => {
+  try {
+    const res = await fetch(API_URL);
+    const { feed } = (await res.json()) as { feed: Feed };
+
+    return feed;
+  } catch (error: any) {
+    throw new Error('Error fetching entries:', error);
+  }
+};
