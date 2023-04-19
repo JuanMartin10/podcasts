ec0b53653c45384cf2e8e7e66bbd176c21a9e680
JuanMrtt <juanantoniomartinb@gmail.com>
2023-04-19 11:40:22 +0200

    feat: persist data for one day

--- src/hooks/useEntries.tsx
+++ src/hooks/useEntries.tsx
@@ -2,11 +2,28 @@ import { useState, useEffect } from 'react';
 import { fetchFeed } from '../services';
 import { type AppEntry } from '../models/types';
 import { useAppContext } from '../context/app-context';
+import { useEntriesStorage, useExpirationStorage } from './useLocalStorage';
 
 export const useEntries = () => {
   const [entries, setEntries] = useState<AppEntry[] | undefined>();
   const { loading, setLoading } = useAppContext();
+  const [expirationValue, setExpirationValue] = useExpirationStorage();
+  const [entriesStorage, setEntriesStorage] = useEntriesStorage();
 
+  const checkHour = () => {
+    const hours = 24;
+    const now = new Date().getTime();
+    if (
+      expirationValue === '' ||
+      now - expirationValue > hours * 60 * 60 * 1000
+    ) {
+      localStorage.clear();
+      setExpirationValue(now);
+      getEntries();
+    } else {
+      setEntries(entriesStorage);
+    }
+  };
   const getEntries = async () => {
     try {
       setLoading(true);
@@ -17,6 +34,7 @@ export const useEntries = () => {
         artist: entry['im:artist'].label,
         image: entry['im:image'][0].label,
       }));
+      setEntriesStorage(mappedEntries);
       setEntries(mappedEntries);
     } catch (error) {
       console.error(error);
@@ -25,7 +43,7 @@ export const useEntries = () => {
     }
   };
   useEffect(() => {
-    getEntries();
+    checkHour();
   }, []);
 
   return { entries, loading, getEntries, setEntries };
